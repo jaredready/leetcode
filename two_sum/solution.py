281a4d09786faf4f2e6b2c4956824e16649ce93b
jaredready <3362733+jaredready@users.noreply.github.com>
2021-02-18 21:30:31 -0600

    Optimize two_sum to O(2n)

--- two_sum/solution.py
+++ two_sum/solution.py
@@ -1,11 +1,9 @@
 class Solution:
   def solve(self, nums: [int], target: int) -> [int]:
-    i, j = 0, 1
-    while i < len(nums):
-      while j < len(nums):
-        if nums[i] + nums[j] == target:
-          return [i, j]
-        else:
-          j += 1
-      i += i
+    map = dict(zip(nums, range(0, len(nums))))
+    for i, num in enumerate(nums):
+      complement = target - num
+      if complement in map and map[complement] != i:
+        return [i, map[complement]]
+    
     return [0,0]
